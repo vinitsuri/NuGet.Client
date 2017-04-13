5fb4b369d1e7a65779371f5d3fd08aaa664d61da
Ankit Mishra <anmishr@microsoft.com>
2017-04-12 17:08:40 -0700

    Adding tests for package filter

--- test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/XPlatAddPkgTests.cs
+++ test/NuGet.Core.FuncTests/NuGet.XPlat.FuncTest/XPlatAddPkgTests.cs
@@ -85,7 +85,7 @@ namespace NuGet.XPlat.FuncTest
                 .ReturnsAsync(0);
 
             testApp.Name = "dotnet nuget_test";
-            AddPackageReferenceCommand.Register(testApp,
+            ListPackageReferenceCommand.Register(testApp,
                 () => logger,
                 () => mockCommandRunner.Object);
 
