a45bdf74306fa19105297bf0bee2ee4ad7d573f9
Justin Emgarten <justin@emgarten.com>
2017-05-01 23:42:48 -0700

    Read API

---
+++ src/NuGet.Core/NuGet.Common/Errors/IFileSectionContext.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGet.Common
+{
+    /// <summary>
+    /// Represents a sub section of a file by marking
+    /// the start line and column and the end line and
+    /// column.
+    /// </summary>
+    public interface IFileSectionContext
+    {
+        /// <summary>
+        /// Full path to the file.
+        /// </summary>
+        string FilePath { get; }
+
+        /// <summary>
+        /// Start line number.
+        /// </summary>
+        int LineNumber { get; }
+
+        /// <summary>
+        /// Start column number.
+        /// </summary>
+        int ColumnNumber { get; }
+
+        /// <summary>
+        /// End line number.
+        /// </summary>
+        int EndLineNumber { get; }
+
+        /// <summary>
+        /// End column number.
+        /// </summary>
+        int EndColumnNumber { get; }
+    }
+}
---
+++ src/NuGet.Core/NuGet.ProjectModel/LockFile/IAssetsLogMessage.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using NuGet.Common;
+
+namespace NuGet.ProjectModel
+{
+    public interface IAssetsLogMessage
+    {
+        /// <summary>
+        /// Warning level.
+        /// </summary>
+        LogLevel Level { get; }
+
+        /// <summary>
+        /// Warning severity.
+        /// </summary>
+        int WarningLevel { get; }
+
+        /// <summary>
+        /// NU warning or error code.
+        /// </summary>
+        NuGetLogCode Code { get; }
+
+        /// <summary>
+        /// Message text.
+        /// </summary>
+        string Message { get; }
+
+        /// <summary>
+        /// Project or Package Id.
+        /// </summary>
+        /// <remarks>Library Ids are always case insensitive.</remarks>
+        string LibraryId { get; }
+
+        /// <summary>
+        /// Path to the project file.
+        /// </summary>
+        string ProjectPath { get; }
+
+        /// <summary>
+        /// File path and starting line, column.
+        /// </summary>
+        IFileSectionContext FileSectionContext { get; }
+
+        /// <summary>
+        /// Target graphs the message applies to.
+        /// If empty this applies to all graphs.
+        /// </summary>
+        IReadOnlyList<string> TargetGraphs { get; }
+    }
+}
