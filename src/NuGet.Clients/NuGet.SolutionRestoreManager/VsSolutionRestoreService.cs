6df7fb421449504dd0f73ba54a092763352120d6
Nikolche Kolev <nikolev@microsoft.com>
2017-06-21 16:19:21 -0700

    Revert "AssetTargetFallback invalid combination error (#1503)"
    This reverts commit 9cc5ab35f3dea4ab3ddb6bb099dabfe2627ab7d0.

--- src/NuGet.Clients/NuGet.SolutionRestoreManager/VsSolutionRestoreService.cs
+++ src/NuGet.Clients/NuGet.SolutionRestoreManager/VsSolutionRestoreService.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -404,7 +404,7 @@ namespace NuGet.SolutionRestoreManager
                                           .ToList();
 
             // Update TFI with fallback properties
-            PackageSpecUtility.ApplyFallbackFramework(tfi, ptf, atf);
+            AssetTargetFallbackUtility.ApplyFramework(tfi, ptf, atf);
 
             if (targetFrameworkInfo.PackageReferences != null)
             {
